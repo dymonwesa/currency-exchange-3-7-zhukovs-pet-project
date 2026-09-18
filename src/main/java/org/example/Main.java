02e0fdb51eb412ab785a2fce11aef4fda515d43b
Smirnov Dmitriy <d.smirnov2006@mail.ru>
2026-09-18 18:44:09 +0300

    added output of db

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,7 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.nio.channels.SelectableChannel;
+import java.sql.*;
 import java.util.MissingFormatArgumentException;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -14,13 +12,26 @@ public class Main {
     public final String url= "jdbc:postgresql://localhost:5432/currency_exchange";
     public final String login= "postgres";
     public final String password= "postgres";
+    public final String SELECT = "SELECT * FROM currencies";
 
 
     public static void main(String[] args) {
         Main main = new Main();
 
-        try (Connection conn = DriverManager.getConnection(main.url, main.login, main.password)) {
-            System.out.println("okay");
+        try (Connection conn = DriverManager.getConnection(main.url, main.login, main.password);
+        var statement = conn.createStatement()) {
+
+            ResultSet resultSet = statement.executeQuery(main.SELECT);
+            while (resultSet.next()){
+                int id = resultSet.getInt("id");
+                String code = resultSet.getString("code");
+                String fullname = resultSet.getString("fullname");
+                String sign = resultSet.getString("sign");
+
+                System.out.println(id + " " + code + " " + fullname + " " + sign);
+
+
+            }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
