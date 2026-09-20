e012b868c40d5dd0732cabd2f91a2d58009dfa32
Smirnov Dmitriy <d.smirnov2006@mail.ru>
2026-09-21 01:43:29 +0300

    upd findAll method

--- src/main/java/org/example/dao/CurrencyDAO.java
+++ src/main/java/org/example/dao/CurrencyDAO.java
@@ -1,20 +1,42 @@
 package org.example.dao;
 
-import com.sun.jdi.connect.spi.Connection;
 import org.example.model.Currency;
+import org.example.util.ConnectionManager;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CurrencyDAO implements Dao<Integer, Currency>{
 
-    private static final CurrencyDAO INSTANCE = new CurrencyDAO();
+    public static final CurrencyDAO INSTANCE = new CurrencyDAO();
+
+    public static final String FIND_ALL = """
+            SELECT *
+            FROM currencies;
+            """;
+
     private CurrencyDAO(){
     }
 
 
     @Override
     public List<Currency> findAll() {
-
-        return List.of();
+        List<Currency> currencies = new ArrayList<>();
+        try (var connection = ConnectionManager.get()){
+            var preparedStatement = connection.prepareStatement(FIND_ALL);
+            var resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                currencies.add(new Currency(
+                        resultSet.getInt("id"),
+                        resultSet.getString("code"),
+                        resultSet.getString("fullname"),
+                        resultSet.getString("sign")
+                        ));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return currencies;
     }
 }
--- src/main/java/org/example/util/ConnectionManager.java
+++ src/main/java/org/example/util/ConnectionManager.java
@@ -19,7 +19,7 @@ public class ConnectionManager {
 
     private static void loadDriver() {
         try{
-            Class.forName("org.postgresql.Driver")
+            Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
